6e266dc992f5e13e3852110f0f137419fe1ef1e9
Brendan O'Brien <titaniumcranium@icloud.com>
2021-02-10 23:36:51 +1100

    refactored frame model to accept user entry for pins
    modified bowl method to accept argument and apply
    score to the correct location
    
    added complete attribute to model to assist with
    the game model picking which frame is currently active
    and handle the endgame
    
    endgame logic now embedded in the different roll
    methods

--- lib/frame.rb
+++ lib/frame.rb
@@ -1,46 +1,48 @@
 # frozen_string_literal: true
 
 class Frame
-  attr_accessor :id, :ball_one, :ball_two, :ball_three
+  attr_accessor :id, :ball_one, :ball_two, :ball_three, :complete
 
   def initialize(id:)
     @id = id
     @ball_one = nil
     @ball_two = nil
     @ball_three = nil
+    @complete = false
   end
 
-  def bowl
-    roll_ball_one
-    roll_ball_two unless @ball_one == 10
-    endgame if id == 10
+  def bowl(pins)
+    return if complete
+
+    if @ball_one.nil?
+      roll_ball_one(pins)
+    elsif @ball_two.nil?
+      roll_ball_two(pins)
+    else
+      roll_ball_three(pins)
+    end
   end
 
   def results
     [@ball_one, @ball_two, @ball_three].compact
   end
 
-  def roll_ball_one
-    @ball_one = roll(pins: 10)
+  def roll_ball_one(pins)
+    @ball_one = pins
+    @complete = true if @ball_one == 10 && id != 10
   end
 
-  def roll_ball_two
-    pins = 10 - @ball_one
-    @ball_two = roll(pins: pins)
-  end
-
-  def roll(pins:)
-    rand(0..pins)
-  end
-
-  private
-
-  def endgame
-    if @ball_one == 10
-      roll_ball_two
-      @ball_three = @ball_two == 10 ? roll(pins: 10) : roll(pins: 10 - @ball_two)
-    elsif @ball_one + @ball_two == 10
-      @ball_three = roll(pins: 10)
+  def roll_ball_two(pins)
+    @ball_two = pins
+    if id == 10
+      @complete = true if @ball_one + @ball_two < 10
+    else
+      @complete = true
     end
+  end
+
+  def roll_ball_three(pins)
+    @ball_three = pins
+    @complete = true
   end
 end
--- spec/frame_spec.rb
+++ spec/frame_spec.rb
@@ -6,16 +6,83 @@ require_relative '../lib/frame'
 RSpec.describe Frame do
   let(:frame) { Frame.new(id: 1) }
   describe '#bowl' do
-    it 'sets the ball_one instance variables' do
-      frame.bowl
-      expect(frame.ball_one).to_not be(nil)
+    context 'frame is open' do
+      let(:pins) { 6 }
+      it 'sets the ball_one score' do
+        frame.bowl(pins)
+        expect(frame.ball_one).to be(pins)
+        expect(frame.complete).to be(false)
+      end
     end
-  end
 
-  describe '#roll' do
-    let(:pins) { 5 }
-    it 'returns an int between 0 and pins' do
-      expect(frame.roll(pins: pins)).to be_between(0, pins)
+    context 'ball one is a strike' do
+      let(:pins) { 10 }
+      it 'sets the ball_one score' do
+        frame.id = 3
+        frame.bowl(pins)
+        expect(frame.ball_one).to be(pins)
+        expect(frame.complete).to be(true)
+      end
+    end
+
+    context 'ball one is a strike in frame 10' do
+      let(:pins) { 10 }
+      it 'sets the ball_one score' do
+        frame.id = 10
+        frame.bowl(pins)
+        expect(frame.ball_one).to be(pins)
+        expect(frame.complete).to be(false)
+      end
+    end
+
+    context 'ball one is already rolled' do
+      let(:pins) { 6 }
+      it 'sets the ball_two score' do
+        frame.ball_one = 3
+        frame.bowl(pins)
+        expect(frame.ball_two).to be(6)
+        expect(frame.complete).to be(true)
+      end
+    end
+
+    context 'ball one and two are are strikes in frame 10' do
+      it 'sets the ball_three score' do
+        frame.id = 10
+        3.times do
+          frame.bowl(10)
+        end
+        expect(frame.ball_three).to be(10)
+      end
+    end
+
+    context 'sum of ball one and two are < 10 in frame 10' do
+      it 'sets the ball_three score' do
+        frame.id = 10
+        2.times do
+          frame.bowl(3)
+        end
+        expect(frame.complete).to be(true)
+      end
+    end
+
+    context ' ball one and two make a spare in frame 10' do
+      it 'keeps the frame open' do
+        frame.id = 10
+        2.times do
+          frame.bowl(5)
+        end
+        expect(frame.complete).to be(false)
+      end
+    end
+
+    context 'a spare in frame 10' do
+      it 'allows ball three to be set' do
+        frame.id = 10
+        3.times do
+          frame.bowl(5)
+        end
+        expect(frame.ball_three).to be(5)
+      end
     end
   end
 
@@ -30,7 +97,7 @@ RSpec.describe Frame do
     end
 
     context 'when the frame is a spare' do
-      it 'returns an array [ball_one, ball_two' do
+      it 'returns an array [ball_one, ball_two]' do
         frame.ball_one = 2
         frame.ball_two = 8
         expect(frame.results).to eq([2, 8])
@@ -41,6 +108,16 @@ RSpec.describe Frame do
       it 'returns a single array, value 10' do
         frame.ball_one = 10
         expect(frame.results).to eq([10])
+      end
+    end
+
+    context 'when frame 10 includes a spare' do
+      it 'returns an array of three values' do
+        frame.id = 10
+        3.times do
+          frame.bowl(5)
+        end
+        expect(frame.results).to eq([5,5,5])
       end
     end
   end
